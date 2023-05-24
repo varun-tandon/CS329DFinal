2843fd4b26938c36ccc6d616e73a1308a253f33e
Avi Goyal <sagoyal@stanford.edu>
2023-05-24 01:40:17 -0700

    changing back to old dqn

--- doubledqn.py
+++ doubledqn.py
@@ -32,36 +32,6 @@ class DoubleDQNAgent():
     def get_best_action(self, state):
         return self.policy_net(state).max(1)[1].view(1, 1)
 
-    # def optimize_model(self, memory):
-    #     if len(memory) < BATCH_SIZE:
-    #         return
-    #     transitions = memory.sample(BATCH_SIZE)
-    #     batch = Transition(*zip(*transitions))
-
-    #     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-    #                                         batch.next_state)), device=device, dtype=torch.bool)
-    #     non_final_next_states = torch.cat([s for s in batch.next_state
-    #                                                 if s is not None])
-    #     state_batch = torch.cat(batch.state)
-    #     action_batch = torch.cat(batch.action)
-    #     reward_batch = torch.cat(batch.reward)
-
-    #     state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-
-    #     next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    #     with torch.no_grad():
-    #         next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
-
-    #     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    #     criterion = nn.SmoothL1Loss()
-    #     loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    #     self.optimizer.zero_grad()
-    #     loss.backward()
-    #     torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
-    #     self.optimizer.step()
-    
     def optimize_model(self, memory):
         if len(memory) < BATCH_SIZE:
             return
@@ -78,20 +48,50 @@ class DoubleDQNAgent():
 
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
-        next_state_values = torch.zeros_like(state_action_values, device=device)
+        next_state_values = torch.zeros(BATCH_SIZE, device=device)
         with torch.no_grad():
-            non_final_next_state_actions = self.target_net(non_final_next_states).max(1)[1].unsqueeze(-1)
-            next_state_values[non_final_mask]  = self.policy_net(non_final_next_states).gather(1, non_final_next_state_actions)
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
 
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
         criterion = nn.SmoothL1Loss()
-        loss = criterion(state_action_values, expected_state_action_values)
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
         self.optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
         self.optimizer.step()
+    
+    # def optimize_model(self, memory):
+    #     if len(memory) < BATCH_SIZE:
+    #         return
+    #     transitions = memory.sample(BATCH_SIZE)
+    #     batch = Transition(*zip(*transitions))
+
+    #     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+    #                                         batch.next_state)), device=device, dtype=torch.bool)
+    #     non_final_next_states = torch.cat([s for s in batch.next_state
+    #                                                 if s is not None])
+    #     state_batch = torch.cat(batch.state)
+    #     action_batch = torch.cat(batch.action)
+    #     reward_batch = torch.cat(batch.reward)
+
+    #     state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+    #     next_state_values = torch.zeros_like(state_action_values, device=device)
+    #     with torch.no_grad():
+    #         non_final_next_state_actions = self.target_net(non_final_next_states).max(1)[1].unsqueeze(-1)
+    #         next_state_values[non_final_mask]  = self.policy_net(non_final_next_states).gather(1, non_final_next_state_actions)
+
+    #     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    #     criterion = nn.SmoothL1Loss()
+    #     loss = criterion(state_action_values, expected_state_action_values)
+
+    #     self.optimizer.zero_grad()
+    #     loss.backward()
+    #     torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+    #     self.optimizer.step()
 
 
     def update_target_network_weights(self):
