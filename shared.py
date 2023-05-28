09315c3ebdfd4f6c57d8eb8f67da9d8c5d2382ae
Varun Tandon <vrntandon@gmail.com>
2023-05-28 12:09:18 -0700

    increase depth of network

--- shared.py
+++ shared.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import torch
 
 SHOULD_PLOT = True
-SHOULD_GENERATE_ADV = True
+SHOULD_GENERATE_ADV = False 
 ADV_GAMMA = 1
 BATCH_SIZE = 64
 GAMMA = 0.99
@@ -10,7 +10,7 @@ EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 1000
 TAU = 0.005
-LR = 1e-4
+LR = 5e-5
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
